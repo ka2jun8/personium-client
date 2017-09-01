3a3f139bdbcb3db2bcc0280074bd8785d0246a1f
kani <kani.junya@jp.fujitsu.com>
2017-09-01 18:21:21 +0900

    build

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -45,9 +45,10 @@ export declare class PersoniumClient {
     getExtCellList(cell: string, _token?: string): Promise<ExtCell[]>;
     sendMessage(cell: string, to: string, type: string, requestContent: string, _token?: string): Promise<PersoniumProfileResponse>;
     getProfile(cell: string): Promise<PersoniumProfileResponse>;
-    get(cell: string, path: string, _token?: string): Promise<PersoniumData | PersoniumData[]>;
-    post(cell: string, path: string, entity: any, _token?: string): Promise<PersoniumData | PersoniumData[]>;
-    update(cell: string, path: string, id: string, entity: any, _token?: string): Promise<PersoniumData | PersoniumData[]>;
+    isExist(cell: string, path: string, _token?: string): Promise<boolean>;
+    get(cell: string, path: string, _token?: string): Promise<PersoniumData[]>;
+    post(cell: string, path: string, entity: any, _token?: string): Promise<any>;
+    update(cell: string, path: string, id: string, entity: any, _token?: string): Promise<any>;
     createCellSchema(cell: string): string;
     extractCellName(url: string): string;
 }
