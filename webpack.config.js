7547d03acd93da9a979abc42f087f885c0ce84a7
kani <kani.junya@jp.fujitsu.com>
2017-09-06 13:45:13 +0900

    webライブラリ用にdistをbuild

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,56 @@
 var path = require("path");
 var webpack = require("webpack");
 
-module.exports = {
-    entry: {
-      'bundle': './src/client.ts',
+module.exports = [
+    {
+        entry: {
+            entry: './src/client.ts'
+        },
+        output: {
+            path: __dirname + "/dist/",
+            filename: "personium-client.js",
+            libraryTarget: "var"
+        },
+        devtool: "#source-map",
+        resolve: {
+            extensions: ['.ts', '.js'],
+        },
+        module: {
+            rules: [{
+                test: /\.ts(x?)$/,
+                use: [
+                    { loader: "tslint-loader" },
+                    { loader: "ts-loader" }
+                ]
+            }]
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                PACKAGE_VERSION: JSON.stringify(require("./package.json").version)
+            }),
+        ],
     },
-    output: {
-        path: __dirname,
-        filename: './lib/personium-client.js'
-    },
-    module: {
-      rules: [{
-        test: /\.ts(x?)$/,
-        use: [
-          {  loader: "tslint-loader"},
-          {  loader: "ts-loader"}
-        ]
-      }]
-    },
-    externals: {
-    },
-    resolve: {
-      extensions: ['.js', '.ts']
-    },
-    devtool: 'inline-source-map'
-};
+    {
+        entry: {
+            'bundle': './src/client.ts',
+        },
+        output: {
+            path: __dirname,
+            filename: './lib/personium-client.js'
+        },
+        module: {
+            rules: [{
+                test: /\.ts(x?)$/,
+                use: [
+                    { loader: "tslint-loader" },
+                    { loader: "ts-loader" }
+                ]
+            }]
+        },
+        externals: {
+        },
+        resolve: {
+            extensions: ['.js', '.ts']
+        },
+        devtool: 'inline-source-map'
+    }];
